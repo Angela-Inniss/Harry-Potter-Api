a5358dc7a8a225086d0be7a1f43211e48721393d
Angela <angela.inniss@justpark.com>
2020-05-11 11:42:56 +0100

    Add main css and index first draft files:

--- js/main.js
+++ js/main.js
@@ -19,15 +19,12 @@ axios.get('http://hp-api.herokuapp.com/api/characters')
     // handle success
     console.log(response);
     response.data.forEach((character) => {
-      // console.log(character);
-      // console.log(character.name);
      renderCharacters(character);
-
     })
   })
   .catch((error) =>  {
     // handle error
-    // console.log(error);
+    console.log(error);
   })
   .then(() =>  {
     // always executed
@@ -35,19 +32,9 @@ axios.get('http://hp-api.herokuapp.com/api/characters')
 
 
 function renderCharacters(character){
-  console.log("hi");
-  console.log(character);
+
   let allCharactersContainer = document.getElementById('hp-container');
   let charactersContainer = document.createElement("div"); //div will be used to hold the data/details for indiviual pokemon.{}
-
-  let characterName = document.createElement('h4');
-  characterName.innerText = ` Name: ${character.name}`;
-
-  let characterPatronus = document.createElement('h4');
-  characterPatronus.innerText = `Patronus: ${character.patronus}`;
-
-  let characterHouse = document.createElement('h4');
-  characterHouse.innerText = `House: ${character.house}`;
 
   // create a span
   var characterImage = document.createElement("span");
@@ -60,8 +47,17 @@ function renderCharacters(character){
   // add created image to the span
   characterImage.appendChild(img);
 
+  let characterName = document.createElement('h4');
+  characterName.innerText = ` Name: ${character.name}`;
+
+  let characterPatronus = document.createElement('h4');
+  characterPatronus.innerText = `Patronus: ${character.patronus}`;
+
+  let characterHouse = document.createElement('h4');
+  characterHouse.innerText = `House: ${character.house}`;
+
 // helper function to go through the types array and create li tags for each one
-  charactersContainer.append(characterName, characterPatronus, characterHouse,characterImage);
+  charactersContainer.append(characterImage, characterName, characterPatronus, characterHouse,);
 // appending all details to the pokeContainer div which was created
   allCharactersContainer.appendChild(charactersContainer);
 // appending that characters container  div to the main div which will hold all the hp cards
